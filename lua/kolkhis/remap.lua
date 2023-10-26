4cabfba33e3704676686d3e981ae297e323abfc0
Kolkhis <36500473+Kolkhis@users.noreply.github.com>
2023-10-26 16:28:04 +0000

    Fix current buffer fuzzy find deprecation warning

--- lua/kolkhis/remap.lua
+++ lua/kolkhis/remap.lua
@@ -40,13 +40,11 @@ require('telescope').setup {
 -- See `:help telescope.builtin`
 vim.keymap.set('n', '<leader>?', builtin.oldfiles, { desc = '[?] Find recently opened files' })
 vim.keymap.set('n', '<leader><space>', builtin.buffers, { desc = '[ ] Find existing buffers' })
-vim.keymap.set('n', '<leader>/', function()
-  -- You can pass additional configuration to telescope to change theme, layout, etc.
-  require('telescope.builtin').current_buffer_fuzzy_find(require('telescope.themes').get_dropdown {
-    winblend = 10,
-    previewer = false,
-  })
-end, { desc = '[/] Fuzzily search in current buffer' })
+vim.keymap.set('n', '<leader>/', builtin.current_buffer_fuzzy_find, { desc = '[/] Fuzzily search in current buffer' })
+-- function()
+--   -- You can pass additional configuration to telescope to change theme, layout, etc.
+--   require('telescope.builtin').current_buffer_fuzzy_find(require('telescope.themes').get_dropdown { winblend = 10, previewer = false, })
+-- end, { desc = '[/] Fuzzily search in current buffer' })
 -- [[ Basic Keymaps ]]
 
 -- Keymaps for better default experience
