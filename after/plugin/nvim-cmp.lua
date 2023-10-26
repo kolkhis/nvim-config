de774e0f6b45f1523e7d25f6c84ed4c2a8e9ecf2
Kolkhis <36500473+Kolkhis@users.noreply.github.com>
2023-10-26 16:28:04 +0000

    Add buffer-sourced completion for search

--- after/plugin/nvim-cmp.lua
+++ after/plugin/nvim-cmp.lua
@@ -1,7 +1,16 @@
 local cmp = require('cmp')
 
 cmp.setup({
-    mapping = cmp.mapping.preset.insert({
-	['<Tab>'] = cmp.mapping.confirm({ select = true }),
-    })
+  mapping = cmp.mapping.preset.insert({
+    ['<Tab>'] = cmp.mapping.confirm({ select = true }),
+  }),
 })
+
+-- Buffer completions for search
+cmp.setup.cmdline({ '/', '?' }, {
+  mapping = cmp.mapping.preset.cmdline(),
+  sources = {
+    { name = 'buffer' },
+  },
+})
+
