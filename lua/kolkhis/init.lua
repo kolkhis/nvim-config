fd0e83e0207eba517a8c67afaa37bf29fa4cee7b
Kolkhis <36500473+Kolkhis@users.noreply.github.com>
2023-10-26 16:28:05 +0000

    Fix streamer mode require

--- lua/kolkhis/init.lua
+++ lua/kolkhis/init.lua
@@ -2,5 +2,5 @@ require('kolkhis.remap')
 require('kolkhis.set')
 require('telescope').setup({})
 require('bufferline')
-require('streamer-mode').setup({ preset = true, default_state = 'off' })
+require('streamer-mode').setup({ default_mode = 'off' })
 
