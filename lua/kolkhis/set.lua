dd14c25026f07537f99605727b026202462b3df4
Kolkhis <36500473+Kolkhis@users.noreply.github.com>
2023-10-26 16:28:04 +0000

    Move highlight on yank from remap to set

--- lua/kolkhis/set.lua
+++ lua/kolkhis/set.lua
@@ -68,3 +68,14 @@ vim.o.textwidth = 79	-- cuz PEP8 lol
 -- vim.o.wrapmargin = -1000 -- Attempt to make python format correctly with `gq`
 
 
+-- [[ Highlight on yank ]]
+-- See `:help vim.highlight.on_yank()`
+local highlight_group = vim.api.nvim_create_augroup("YankHighlight", { clear = true })
+vim.api.nvim_create_autocmd("TextYankPost", {
+	callback = function()
+		vim.highlight.on_yank()
+	end,
+	group = highlight_group,
+	pattern = "*",
+})
+
