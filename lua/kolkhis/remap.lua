782e4717f81d793172890ba1830937e85132e6fa
Kolkhis <36500473+Kolkhis@users.noreply.github.com>
2023-10-26 16:28:04 +0000

    Remap jumps and <Esc> using zu, zd, and zj respectively

--- lua/kolkhis/remap.lua
+++ lua/kolkhis/remap.lua
@@ -3,9 +3,9 @@ vim.keymap.set({ "n", "v" }, "<Space>", "<Nop>", { silent = true })
 vim.g.mapleader = " "
 vim.g.maplocalleader = " "
 
-vim.keymap.set({ "n", "v" }, "zj", "<C-d>", { silent = true }) -- jump down
-vim.keymap.set({ "n", "v" }, "zk", "<C-u>", { silent = true }) -- jump up
-vim.keymap.set({ "i", "v" }, "zl", "<Esc>", { silent = false, noremap = true }) -- remap escape key
+vim.keymap.set({ "n", "v" }, "zu", "<C-d>", { silent = true }) -- jump down
+vim.keymap.set({ "n", "v" }, "zd", "<C-u>", { silent = true }) -- jump up
+vim.keymap.set({ "i", "v" }, "zj", "<Esc>", { silent = false, noremap = true }) -- remap escape key
 -- vim.keymap.set( 'n', 'k', 'gk', { silent = true, noremap = true })
 
 -- Remap for dealing with word wrap
