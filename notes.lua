7ba59d144b9fd0fbcf4a71b2f38afa10d791d432
Kolkhis <36500473+Kolkhis@users.noreply.github.com>
2023-10-26 16:28:04 +0000

    Add global install registry notes for Windows

--- notes.lua
+++ notes.lua
@@ -4,6 +4,13 @@ Windows Registry Editor Version 5.00
 
 [HKEY_CLASSES_ROOT\*\shell\Edit with Neovim]
 "Icon"="C:\Users\username\AppData\Local\Programs\Neovim\bin\nvim-qt.exe"
+OR, if installed globally:
+"Icon"="C:\Program Files\Neovim\bin\nvim-qt.exe"
 
 [HKEY_CLASSES_ROOT\*\shell\Edit with Neovim\command]
-@="C:\Users\username\AppData\Local\Programs\Neovim\bin\nvim-qt.exe" 
+@="C:\Users\username\AppData\Local\Programs\Neovim\bin\nvim-qt.exe" "%1"
+OR, if installed globally:
+@="C:\Program Files\Neovim\bin\nvim-qt.exe" "%1"
+
+--]]
+
