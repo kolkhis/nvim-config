2e4f3a8516e7ed76ab7af262815e774c0fa31c75
Kolkhis <36500473+Kolkhis@users.noreply.github.com>
2023-11-07 21:20:37 -0500

    fix: Move function definitions to keymaps directly

--- after/plugin/sourcegraph.lua
+++ after/plugin/sourcegraph.lua
@@ -15,24 +15,31 @@ if os.is_linux then
 
   local function cody_task()
     local mode = vim.api.nvim_get_mode()
-    if mode == 'v' then
-      vim.ui.input({ prompt = 'Cody Task > ' }, function(task)
-        vim.cmd((":'<,'>CodyTask %s"):format(task))
-      end)
-    elseif mode == 'n' then
+    if mode == 'n' then
       vim.ui.input({ prompt = 'Cody Task (global) > ' }, function(task)
         vim.cmd((':%CodyTask %s'):format(task))
+      end)
+    else
+      vim.ui.input({ prompt = 'Cody Task > ' }, function(task)
+        vim.cmd((":'<,'>CodyTask %s"):format(task))
       end)
     end
   end
   -- -- TODO: Get prompt
-  vim.keymap.set('v', '<leader>ca', ask_cody, { noremap = true, desc = "Ask Cody about the current selection" })
+  -- vim.keymap.set('v', '<leader>ca', ask_cody, { noremap = true, desc = "Ask Cody about the current selection" })
 
-  -- vim.keymap.set('v', '<leader>ca', function()
-  --   vim.ui.input({ prompt = 'Ask Cody about selection > ' }, function(question)
-  --     vim.cmd((":'<,'>CodyAsk %s"):format(question))
-  --   end)
-  -- end)
+  vim.keymap.set({ 'n', 'v' }, '<leader>ca', function()
+    local mode = vim.api.nvim_get_mode()
+    if mode.mode == 'n' then
+      vim.ui.input({ prompt = 'Ask Cody about file > ' }, function(question)
+        vim.cmd((':0,$CodyAsk %s'):format(question))
+      end)
+    else
+      vim.ui.input({ prompt = 'Ask Cody about selection > ' }, function(question)
+        vim.cmd((":'<,'>CodyAsk %s"):format(question))
+      end)
+    end
+  end)
 
   vim.keymap.set(
     { 'n', 'v' },
@@ -41,6 +48,28 @@ if os.is_linux then
     { silent = true, noremap = true, desc = 'Toggle the Cody Chat Window' }
   )
 
-vim.keymap.set({ 'n', 'v' }, '<leader>cp', cody_task, { noremap = true, desc = 'Task [C]ody with a [P]rompt.' })
+  vim.keymap.set({ 'n', 'v' }, '<leader>cp', function () vim.ui.input({ prompt = 'Cody Task ->'}, function (task)
+        vim.cmd((":CodyTask %s"):format(task))
+    end)
+  end)
 
-end
+
+--   vim.keymap.set({ 'n', 'v' }, '<leader>cp', function()
+--     local mode = vim.api.nvim_get_mode()
+--     if mode == 'n' then
+--       vim.ui.input({ prompt = 'Cody Task (global) > ' }, function(task)
+--         vim.cmd((':0;$CodyTask %s'):format(task))
+--       end)
+--     else
+--       vim.ui.input({ prompt = 'Cody Task > ' }, function(task)
+--         vim.cmd((":'<,'>CodyTask %s"):format(task))
+--       end)
+--     end
+--   end, { noremap = true, desc = 'Task [C]ody with a [P]rompt.' })
+-- end
+
+
+
+end -- End of os.is_linux() check
+
+
