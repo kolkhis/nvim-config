c360d669f03e053b9d99f4ab1b664eff60d81cff
Kolkhis <36500473+Kolkhis@users.noreply.github.com>
2024-07-22 14:20:57 -0400

    chore: Remove unused index in for loop

--- lua/kolkhis/packer.lua
+++ lua/kolkhis/packer.lua
@@ -80,7 +80,7 @@ return require('packer').startup(function(use)
             local server_str = ''
             -- Check if servers are already installed
             local mason_registry = require('mason-registry')
-            for i, server in ipairs(language_servers) do
+            for _, server in ipairs(language_servers) do
                 local installed = mason_registry.is_installed(server)
                 if not installed then
                     server_str = ('%s%s '):format(server_str, server) -- server_str .. server .. ' '
