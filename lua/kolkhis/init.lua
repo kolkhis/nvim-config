e51f26b6024369f5812c4e9b44aff8021e7693c1
Kolkhis <36500473+Kolkhis@users.noreply.github.com>
2025-02-12 18:03:09 -0500

    fix: Add check for local streamer-mode.nvim repository before calling setup

--- lua/kolkhis/init.lua
+++ lua/kolkhis/init.lua
@@ -4,10 +4,12 @@ require('telescope').setup({})
 
 local host_os = require('kolkhis.detect_os')
 if host_os.is_linux and not host_os.is_termux then
-    require('streamer-mode').setup({
-        default_state = 'off',
-        level = 'edit',
-        keywords = { 'api_key' },
-        exclude_default_keywords = { 'alias', '$env:', 'export' },
-    })
+    if vim.fn.isdirectory('~/Repos/github.com/kolkhis/streamer-mode.nvim/') then
+        require('streamer-mode').setup({
+            default_state = 'off',
+            level = 'edit',
+            keywords = { 'api_key' },
+            exclude_default_keywords = { 'alias', '$env:', 'export' },
+        })
+    end
 end
