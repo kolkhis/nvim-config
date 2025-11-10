f4636e3ea248768ff81a0199164c93a5b608dc89
Kolkhis <36500473+Kolkhis@users.noreply.github.com>
2025-11-10 14:18:38 -0500

    feat: Add after files for lsp configuration and telescope

---
+++ after/plugin/lsp.lua
@@ -0,0 +1,42 @@
+local host_os = require('kolkhis.detect_os')
+if host_os.is_linux or host_os.is_termux then
+    ConfigPath = vim.fs.normalize('~/.dotfiles/nvim/.config/nvim/stylua.toml')
+else
+    ConfigPath = vim.fs.normalize('E:/Coding/.config/stylua.toml')
+end
+
+vim.lsp.config.lua_ls = {
+    settings = {
+        Lua = {
+            completion = {
+                callSnippet = 'Replace',
+            },
+            workspace = {
+                library = vim.api.nvim_get_runtime_file('', true),
+                checkThirdParty = false, -- Fix for "Configure current file" prompt
+            },
+            formatting = {
+                formatAs = 'lua',
+                styluaArgs = { '--config-path', ConfigPath },
+            },
+        },
+    },
+}
+
+vim.lsp.config.bashls = {
+    cmd = { 'bash-language-server', 'start' },
+    filetypes = { 'sh', 'bash', },
+}
+
+vim.lsp.enable({
+    'lua_ls', 'bashls', 'gopls',
+})
+
+vim.api.nvim_create_autocmd('LspAttach', {
+    callback = function(ev)
+        local client = vim.lsp.get_client_by_id(ev.data.client_id)
+        if client:supports_method('textDocument/completion') then
+            vim.lsp.completion.enable(true, client.id, ev.buf, { autotrigger = true })
+        end
+    end,
+})
