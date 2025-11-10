d1df96c94c60af9ba45be6b8f8272571e7a68f2c
Kolkhis <36500473+Kolkhis@users.noreply.github.com>
2025-11-10 16:55:46 -0500

    chore: Trim treesitter config

--- after/plugin/treesitter.lua
+++ after/plugin/treesitter.lua
@@ -1,5 +1,5 @@
 require('nvim-treesitter.configs').setup({
-    -- A list of parser names, or "all" (the five listed parsers should always be installed)
+    -- :h nvim-treesitter-modules
     -- `:TSInstallInfo` for a list of available parsers
     ensure_installed = {
         'bash',
@@ -68,7 +68,7 @@ require('nvim-treesitter.configs').setup({
 
     highlight = {
         enable = true,
-        -- Or use a function for more flexibility, e.g. to disable slow treesitter highlight for large files
+        -- Disable slow treesitter highlight for large files
         disable = function(lang, buf)
             local max_filesize = 100 * 1024 -- 100 KB
             local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
@@ -77,63 +77,7 @@ require('nvim-treesitter.configs').setup({
             end
         end,
 
-        highlight = { enable = false },
         indent = { enable = false },
-        incremental_selection = {
-            enable = true,
-            keymaps = {
-                init_selection = '<c-space>',
-                node_incremental = '<c-space>',
-                scope_incremental = '<c-s>',
-                node_decremental = '<M-space>',
-            },
-        },
-        textobjects = {
-            select = {
-                enable = true,
-                lookahead = true, -- Automatically jump forward to textobj, similar to targets.vim
-                keymaps = {
-                    -- You can use the capture groups defined in textobjects.scm
-                    ['aa'] = '@parameter.outer',
-                    ['ia'] = '@parameter.inner',
-                    ['af'] = '@function.outer',
-                    ['if'] = '@function.inner',
-                    ['ac'] = '@class.outer',
-                    ['ic'] = '@class.inner',
-                },
-            },
-            move = {
-                enable = true,
-                set_jumps = true, -- whether to set jumps in the jumplist
-                goto_next_start = {
-                    [']m'] = '@function.outer',
-                    [']]'] = '@class.outer',
-                },
-                goto_next_end = {
-                    [']M'] = '@function.outer',
-                    [']['] = '@class.outer',
-                },
-                goto_previous_start = {
-                    ['[m'] = '@function.outer',
-                    ['[['] = '@class.outer',
-                },
-                goto_previous_end = {
-                    ['[M'] = '@function.outer',
-                    ['[]'] = '@class.outer',
-                },
-            },
-
-            swap = {
-                enable = true,
-                swap_next = {
-                    ['<leader>a'] = '@parameter.inner',
-                },
-                swap_previous = {
-                    ['<leader>A'] = '@parameter.inner',
-                },
-            },
-        },
-        additional_vim_regex_highlighting = true,
+        additional_vim_regex_highlighting = false,
     },
 })
--- `:h nvim-treesitter-incremental-selection-mod`
