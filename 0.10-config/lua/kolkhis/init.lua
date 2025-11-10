ca075c5b7ddb34224a9a629d317b00d362b9abc2
Kolkhis <36500473+Kolkhis@users.noreply.github.com>
2025-11-10 12:40:13 -0500

    refactor: Move all old config to dir, start fresh

---
+++ 0.10-config/lua/kolkhis/init.lua
@@ -0,0 +1,15 @@
+require('kolkhis.remap')
+require('kolkhis.set')
+require('telescope').setup({})
+
+local host_os = require('kolkhis.detect_os')
+if host_os.is_linux and not host_os.is_termux then
+    if vim.fn.isdirectory('~/Repos/github.com/kolkhis/streamer-mode.nvim/') then
+        require('streamer-mode').setup({
+            default_state = 'off',
+            level = 'edit',
+            keywords = { 'api_key' },
+            exclude_default_keywords = { 'alias', '$env:', 'export' },
+        })
+    end
+end
