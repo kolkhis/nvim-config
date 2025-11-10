52317a83b3c22a153cef1e4937e98f0cbf296872
Kolkhis <36500473+Kolkhis@users.noreply.github.com>
2025-11-10 15:50:52 -0500

    feat: Add basic treesitter configuration

--- 0.10-config/after/plugin/treesitter.lua
+++ 0.10-config/after/plugin/treesitter.lua
@@ -1,36 +1,34 @@
 require('nvim-treesitter.configs').setup({
-    -- A list of parser names, or "all" (the five listed parsers should always be installed)
     -- `:TSInstallInfo` for a list of available parsers
     ensure_installed = {
         'bash',
         'python',
         'c',
         'lua',
-        'vim',
-        'vimdoc',
-        'javascript',
-        'typescript',
-        'perl',
-        'rust',
-        'cpp',
-        'java',
-        'r',
-        'html',
-        'htmldjango',
-        'css',
-        'sql',
         'go',
         'gomod',
         'gosum',
         'gowork',
+        'vim',
+        'vimdoc',
+        'perl',
+        'cpp',
+        'java',
+        'r',
+        'javascript',
+        'typescript',
+        'rust',
+        'html',
+        'htmldjango',
+        'css',
+        'sql',
         'php',
         'phpdoc',
         'markdown',
         'markdown_inline',
         'comment',
-        'psv',
-        'nix',
         'terraform',
+        'hcl',
 
         'xml',
         'csv',
@@ -79,60 +77,6 @@ require('nvim-treesitter.configs').setup({
 
         highlight = { enable = false },
         indent = { enable = false },
-        incremental_selection = {
-            enable = true,
-            keymaps = {
-                init_selection = '<c-space>',
-                node_incremental = '<c-space>',
-                scope_incremental = '<c-s>',
-                node_decremental = '<M-space>',
-            },
-        },
-        textobjects = {
-            select = {
-                enable = true,
-                lookahead = true, -- Automatically jump forward to textobj, similar to targets.vim
-                keymaps = {
-                    -- You can use the capture groups defined in textobjects.scm
-                    ['aa'] = '@parameter.outer',
-                    ['ia'] = '@parameter.inner',
-                    ['af'] = '@function.outer',
-                    ['if'] = '@function.inner',
-                    ['ac'] = '@class.outer',
-                    ['ic'] = '@class.inner',
-                },
-            },
-            move = {
-                enable = true,
-                set_jumps = true, -- whether to set jumps in the jumplist
-                goto_next_start = {
-                    [']m'] = '@function.outer',
-                    [']]'] = '@class.outer',
-                },
-                goto_next_end = {
-                    [']M'] = '@function.outer',
-                    [']['] = '@class.outer',
-                },
-                goto_previous_start = {
-                    ['[m'] = '@function.outer',
-                    ['[['] = '@class.outer',
-                },
-                goto_previous_end = {
-                    ['[M'] = '@function.outer',
-                    ['[]'] = '@class.outer',
-                },
-            },
-
-            swap = {
-                enable = true,
-                swap_next = {
-                    ['<leader>a'] = '@parameter.inner',
-                },
-                swap_previous = {
-                    ['<leader>A'] = '@parameter.inner',
-                },
-            },
-        },
         additional_vim_regex_highlighting = true,
     },
 })
