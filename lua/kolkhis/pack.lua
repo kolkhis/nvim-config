dc711681534896e17c0fa15b3d4efb17c6cc40e0
Kolkhis <36500473+Kolkhis@users.noreply.github.com>
2025-11-10 15:01:08 -0500

    docs: Add clarifying comments to plugins

--- lua/kolkhis/pack.lua
+++ lua/kolkhis/pack.lua
@@ -3,16 +3,19 @@ vim.pack.add({
     { src = 'https://github.com/tpope/vim-surround' },
     { src = 'https://github.com/tpope/vim-repeat' },
 
+    -- aesthetics
     { src = 'https://github.com/marko-cerovac/material.nvim' },
     { src = 'https://github.com/nvim-treesitter/nvim-treesitter' },
 
+    -- LSP
     { src = 'https://github.com/neovim/nvim-lspconfig' },
     { src = "https://github.com/folke/lazydev.nvim" },
     { src = 'https://github.com/williamboman/mason.nvim' },
     { src = 'https://github.com/fatih/vim-go' },
 
+    -- misc. utils
     { src = 'https://github.com/nvim-telescope/telescope.nvim' },
 })
 
--- To delete packages
--- vim.pack.del({})
+-- To delete plugins
+-- vim.pack.del({'name'})
